e54ad42f467fe49b99ab5aca5e677068871c462a
石磊 <shil-k@glodon.com>
2025-12-18 17:19:14 +0800

    shilei

--- agent_headler/sequence_match_agent.py
+++ agent_headler/sequence_match_agent.py
@@ -68,5 +68,5 @@ if __name__ == '__main__':
     # print(llm.invoke("你是谁？"))
     agent_client = SequenceMatchAgent(text_key="input_text", model=llm)
 
-    res = agent_client.run({"input_text": "楼面5-防滑地砖-其他层\n"})
+    res = agent_client.run({"input_text": "2、最薄处20mm厚C20细石混凝土向地漏找1%坡,随打随抹平,管根做80高混凝土止水台\n"})
     print(f"res {res} type {type(res)}")
